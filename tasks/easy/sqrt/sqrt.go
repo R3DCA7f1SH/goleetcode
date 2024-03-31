9a1185ca38eaa1af8bbe913dcd8659ae63d4cb7d
R3DCA7f1SH <mikhailodinol@gmail.com>
2024-03-31 17:11:38 +0300

    sqrt 0ms yeah boy

--- tasks/easy/sqrt/sqrt.go
+++ tasks/easy/sqrt/sqrt.go
@@ -1,17 +1,12 @@
 package tasks
 
 func mySqrt(x int) int {
-	if x == 1 {
-		return 1
+	if x < 2 {
+		return x
 	}
-	if x == 0 {
-		return 0
-	}
-	num := x
-	root := num / 2
-	eps := 0
-	for root-num/root > eps {
-		root = int((root + num/root) / 2)
+	root := x >> 1
+	for root-x/root > 0 {
+		root = (root + x/root) >> 1
 	}
 	return root
 }
