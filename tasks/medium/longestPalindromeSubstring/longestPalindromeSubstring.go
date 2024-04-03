3315fc600a61bc9b629a4756cb66c52a02278f80
R3DCA7f1SH <mikhailodinol@gmail.com>
2024-04-03 11:52:50 +0300

    longestPalindromeSubstring

--- tasks/medium/longestPalindromeSubstring/longestPalindromeSubstring.go
+++ tasks/medium/longestPalindromeSubstring/longestPalindromeSubstring.go
@@ -1,5 +1,24 @@
 package tasks
 
+//Мое решение не проходило по скорости, это чужое, проанализировать
+func GetPal(s string, left int, right int) string {
+	for left >= 0 && right < len(s) && s[left] == s[right] {
+		left--
+		right++
+	}
+	return s[left+1 : right]
+}
+
 func longestPalindrome(s string) string {
-	return ""
+	var palindrom string
+	for i, _ := range s {
+		op1, op2 := GetPal(s, i, i), GetPal(s, i, i+1)
+		if len(op1) > len(palindrom) {
+			palindrom = op1
+		}
+		if len(op2) > len(palindrom) {
+			palindrom = op2
+		}
+	}
+	return palindrom
 }
